8d8c2fb437a5eeb83c2ef1386e6a2d97694e5dd4
rkonesky <rosskon1023@gmail.com>
2022-01-08 11:48:20 -0600

    backend server build complete

---
+++ controllers/peoples.js
@@ -0,0 +1,58 @@
+// Require Dependencies
+const express = require('express');
+
+// Create a Route Object
+const peoplesRouter = express.Router();
+const People = require('../models/people.js');
+
+
+// localhost 3001
+peoplesRouter.get("/", (req,res) => {
+    res.send('hello world')
+});
+
+// Index
+peoplesRouter.get("/people", async (req,res) => {
+    try {
+        res.json(await People.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+// New
+
+// Delete
+peoplesRouter.delete("/people/:id", async (req,res) => {
+    try {
+        res.json(await People.findByIdAndDelete(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+// Update
+peoplesRouter.put("/people/:id", async (req,res) => {
+    try {
+        res.json(await People.findByIdAndUpdate(req.params.id, req.body, {new:true}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+
+// Create
+peoplesRouter.post("/people", async(req,res) => {
+    try {
+        res.json(await People.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// Edit
+
+// Show 
+
+// Export the Router/Controller Object
+module.exports = peoplesRouter;
---
+++ server.js
@@ -0,0 +1,35 @@
+// Require Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+// Add Controllers
+const peoplesController = require('./controllers/peoples.js');
+
+// Initialize the Application:
+const app = express();
+
+// Configure Application Settings:
+const {PORT = 3001, DATABASE_URL} = process.env;
+
+// Connect to and Configure the Database
+mongoose.connect(DATABASE_URL);
+
+// Set up Database Listeners
+mongoose.connection
+    .on("open", () => console.log("You are connected to MongoDB"))
+    .on("close", () => console.log("You are disconnected from MongoDB"))
+    .on("error", (error) => console.log(error))
+
+// Mount Middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Mount Routes 
+app.use('/', peoplesController);
+
+// Application Listener
+app.listen(PORT, () => console.log(`Server is listening in port: ${PORT}`));
