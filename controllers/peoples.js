28e77d4e64878ddcadbfa3a3c6e95fa270b0d3f0
rkonesky <rosskon1023@gmail.com>
2022-01-23 13:54:05 -0600

    test

--- controllers/peoples.js
+++ controllers/peoples.js
@@ -2,6 +2,10 @@
 const express = require('express');
 
 const admin = require('firebase-admin');
+const serviceAccount = require('../service-account-credentials.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 async function isAuthenticated(req, res, next) {
     try {
